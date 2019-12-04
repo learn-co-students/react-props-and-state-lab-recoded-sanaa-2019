7afd7ab26afcd86f09a1a840558b11835183ab6d
AimanAlmureish <aiman.almureish@gmail.com>
2019-12-04 11:55:17 -0800

    Done.

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,28 +1,56 @@
 import React from 'react'
 
+
 class Pet extends React.Component {
   render() {
-    return (
-      <div className="card">
-        <div className="content">
-          <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
-          </a>
-          <div className="meta">
-            <span className="date">PET TYPE</span>
+    {
+      if (this.props.pet.isAdopted === true) {
+        return (
+          <div className="card">
+            <div className="content">
+              <a className="header">
+                {this.props.pet.gender == 'male' ? '♂' : '♀'/*'♀' OR '♂' */}
+                {this.props.pet.name}
+              </a>
+              <div className="meta">
+                <span className="date">{this.props.pet.type}</span>
+              </div>
+              <div className="description">
+                <p>Age: {this.props.pet.age}</p>
+                <p>Weight: {this.props.pet.weight}</p>
+              </div>
+            </div>
+            <div className="extra content">
+              <button className="ui disabled button">Already adopted</button>
+
+            </div>
           </div>
-          <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+        )
+      } else {
+        return (
+          <div className="card">
+            <div className="content">
+              <a className="header">
+                {this.props.pet.gender == 'male' ? '♂' : '♀'/*'♀' OR '♂' */}
+                {this.props.pet.name}
+              </a>
+              <div className="meta">
+                <span className="date">{this.props.pet.type}</span>
+              </div>
+              <div className="description">
+                <p>Age: {this.props.pet.age}</p>
+                <p>Weight: {this.props.pet.weight}</p>
+              </div>
+            </div>
+            <div className="extra content">
+              <button className="ui primary button" onClick={() => this.props.onAdoptPet(this.props.pet.id)}>Adopt pet</button>
+
+            </div>
           </div>
-        </div>
-        <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
-        </div>
-      </div>
-    )
+        )
+      }
+    }
+
   }
 }
 
